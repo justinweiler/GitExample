392f861c45e300c778d8f0516e059222d22c5825
Justin Weiler <Justin Weiler@LNV4702>
2016-05-14 04:13:26 -0700

    festure 2 finished

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -11,6 +11,7 @@ namespace GitExample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("feature 2");
         }
     }
 }
